ba3b9f258fe83829ac91eef0661ef663cf61aec0
nasty <nasty@DESKTOP-PUV26J0>
2023-03-07 15:13:18 +0700

    Добавьте файлы проекта.

---
+++ gorbatyuk.anastasia.931903.web.lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gorbatyuk.anastasia._931903.web.lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ gorbatyuk.anastasia.931903.web.lab2/Service/CalcService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gorbatyuk.anastasia._931903.web.lab2.Models;
+
+namespace gorbatyuk.anastasia._931903.web.lab2.Service
+{
+    public class CalcService
+    {
+        public static string Convert(Calc calc)
+        {
+            return calc.operation switch
+            {
+                "+" => $"{calc.a} + {calc.b} = {calc.a + calc.b}",
+                "-" => $"{calc.a} - {calc.b} = {calc.a - calc.b}",
+                "*" => $"{calc.a} * {calc.b} = {calc.a * calc.b}",
+                "/" when calc.b != 0 => $"{calc.a} / {calc.b} = {calc.a / calc.b}",
+                "/" when calc.b == 0 => "Ошибка: деление на нуль",
+                _ => "Неизвестная операция"
+            };
+        }
+    }
+}
